b9745696f250c132cb9624335e6649fb733fc9c9
Duane Chan <duanechan19@gmail.com>
2025-10-15 20:41:17 +0800

    Update main.py to add sys.args

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from stats import get_num_words, get_character_counts, sort_character_counts
+import sys
 
 def print_banner(title: str, border: str, left_padding: int, right_padding: int):
     print(border * left_padding, title, border * right_padding)
 
-def get_book_text(filepath: str = "books/frankenstein.txt"):
+def get_book_text(filepath: str):
     print(f"Analyzing book found at {filepath}...")
     with open(filepath) as f:
         return f.read()
@@ -21,10 +22,22 @@ def display_char_count(chars_count):
         print(f"{e["char"]}: {e["num"]}")
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    file_path = sys.argv[1]
+
     print_banner("BOOKBOT", "=", 12, 12)
-    file_contents = get_book_text()
+
+    file_contents = get_book_text(file_path)
+    
     display_word_count(file_contents)
+
     char_count = get_character_counts(file_contents)
+    
     display_char_count(sort_character_counts(char_count))
+    
     print_banner("END", "=", 13, 15)
+
 main()
