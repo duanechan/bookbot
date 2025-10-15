0aef830b88d3f45fa14a8e10bc10883da0edca86
Duane Chan <duanechan19@gmail.com>
2025-10-15 14:37:52 +0800

    Add main.py and stats.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from stats import get_num_words
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    print(get_num_words(file_contents))
+
+main()
