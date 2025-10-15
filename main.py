4ecbe7f28954de3f06744fdda8a0bd57af6a9ee8
Duane Chan <duanechan19@gmail.com>
2025-10-15 20:33:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from stats import get_num_words, count_characters
+from stats import get_num_words, get_character_counts, sort_character_counts
 
-def get_book_text(filepath):
+def print_banner(title: str, border: str, left_padding: int, right_padding: int):
+    print(border * left_padding, title, border * right_padding)
+
+def get_book_text(filepath: str = "books/frankenstein.txt"):
+    print(f"Analyzing book found at {filepath}...")
     with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
+        return f.read()
+
+def display_word_count(file_contents: str):
+    num_words = get_num_words(file_contents)
+    print_banner("Word Count", "-", 11, 10)
+    print(f"Found {num_words} total words")
+
+def display_char_count(chars_count):
+    print("-" * 9, "Character Count", "-" * 7)
+    for e in chars_count:
+        if not e["char"].isalpha():
+            continue
+        print(f"{e["char"]}: {e["num"]}")
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
-    print(count_characters(file_contents))
-
+    print_banner("BOOKBOT", "=", 12, 12)
+    file_contents = get_book_text()
+    display_word_count(file_contents)
+    char_count = get_character_counts(file_contents)
+    display_char_count(sort_character_counts(char_count))
+    print_banner("END", "=", 13, 15)
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,8 @@
-def get_num_words(text):
-    words = set(text.split())
-    return f"Found {len(words)} total words"
+def get_num_words(text: str):
+    words = text.split()
+    return len(words)
 
-def count_characters(text):
+def get_character_counts(text: str):
     char_count = {}
     for c in text:
         lowered = c.lower()
@@ -11,3 +11,16 @@ def count_characters(text):
         else:
             char_count[lowered] = 1
     return char_count
+
+def sort_character_counts(character_counts: dict[str, int]):
+    def sort_on(items: list):
+        return items["num"]
+    
+    sorted_list = []
+
+    for c in character_counts:
+        sorted_list.append({"char": c, "num": character_counts[c]})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
